6110a4ab7cfe68296343ac1396da65b065d02e07
tmahicharan <tmahicharan@gmail.com>
2026-06-14 21:34:11 +0530

    conversation

--- conv.sh
+++ conv.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#person1=Ramesh
-#person2=Suresh
+person1=Suresh
+person2=Ramesh
 
-echo " suresh:: Hi Ramesh, How are you?"
-echo " ramesh:: I am fine, How are you?"
-echo " suresh:: I am fine too, What are you doing?"
-echo " ramesh:: I am learning shell scripting, What about you?"
-echo " suresh:: I am also learning shell scripting, What is your favorite topic?"
+echo " $person1:: Hi $person2, How are you?"
+echo " $person2:: I am fine, How are you?"
+echo " $person1:: I am fine too, What are you doing?"
+echo " $person2:: I am learning shell scripting, What about you?"
+echo " $person1:: I am also learning shell scripting, What is your favorite topic?"
