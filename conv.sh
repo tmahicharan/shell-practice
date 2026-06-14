65e6c397040c64389b47eab64e231fa1406dfa9f
tmahicharan <tmahicharan@gmail.com>
2026-06-14 21:26:43 +0530

    conversation

---
+++ conv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#person1=Ramesh
+#person2=Suresh
+
+echo " suresh:: Hi Ramesh, How are you?"
+echo " ramesh:: I am fine, How are you?"
+echo " suresh:: I am fine too, What are you doing?"
+echo " ramesh:: I am learning shell scripting, What about you?"
+echo " suresh:: I am also learning shell scripting, What is your favorite topic?"
