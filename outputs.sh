53dfe4c3a1a2f83738b52a5a26e242a97984c992
tmahicharan <tmahicharan@gmail.com>
2026-06-15 00:20:12 +0530

    conversation

--- outputs.sh
+++ outputs.sh
@@ -2,5 +2,5 @@
 
 leaders=("Mahatma Gandhi" "Nelson Mandela" "Martin Luther King Jr." "Abraham Lincoln" "Winston Churchill")
 
-echo " All leaders : {$leaders [@]}"
-echo " 2nd leader : {$leaders [2]}"
+echo "All leaders: ${leaders[@]}"
+echo "2nd leader: ${leaders[2]}"
