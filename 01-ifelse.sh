5412edfbccecba1e571e31e792c109ddf36aafed
tmahicharan <tmahicharan@gmail.com>
2026-06-15 17:11:14 +0530

    array

--- 01-ifelse.sh
+++ 01-ifelse.sh
@@ -4,8 +4,8 @@ NUMBER=$1
 
 if [ $NUMBER -lt 10 ]; then
     echo "Entered number is less than 10"
-elif [ $NUMBER -eq 0 ]; then
-    echo "Entered number is equal to zero"
+elif [ $NUMBER -eq 10 ]; then
+    echo "Entered number is equal to 10"
 else
     echo "Entered number is greater than 10"
 fi
