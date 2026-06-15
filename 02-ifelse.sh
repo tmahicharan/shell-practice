77b8e947e5e7bec795a9e71fe8dd62f88c4419f4
tmahicharan <tmahicharan@gmail.com>
2026-06-15 17:21:12 +0530

    array

--- 02-ifelse.sh
+++ 02-ifelse.sh
@@ -2,7 +2,7 @@
 
 echo "Please enter a number:"
 read NUMBER
-if [ $NUMBER %2 -eq 0 ]; then
+if [ $(($NUMBER %2)) -eq 0 ]; then
     echo "Entered number is even"
 else
     echo "Entered number is odd"
