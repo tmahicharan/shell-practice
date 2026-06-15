5774ba29125091fb0120c9c7be2b7b1258d69f82
tmahicharan <tmahicharan@gmail.com>
2026-06-15 17:06:18 +0530

    array

--- outputs.sh
+++ outputs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-persons=("mahi" "vyshu" "divya" "sai")
+persons="mahi","vyshu","divya","sai"
 echo "All persons are : ${persons[@]}"
-echo "Frist person is : ${persons[1]}"
+echo "First person is : ${persons[0]}"
